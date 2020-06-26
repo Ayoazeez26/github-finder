85b9ea217e00e2bb23374fb5af2af66b481b12ee
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2020-06-26 13:39:58 +0100

    initial and final commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class GitHub {
+    constructor() {
+        this.client_id = '2b3af783db9be3a2c556';
+        this.client_secret = '117f4ae71f03a0f966e0e6fc4bb6e1bc3413405d';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
